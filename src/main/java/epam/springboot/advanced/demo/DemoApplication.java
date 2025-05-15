345db492e146ead1716fcd60dc9be81bae740b2a
Aliaksei_Kavaliou1 <aliaksei_kavaliou1@epam.com>
2025-05-15 18:02:25 +0300

    Subtask_4

--- src/main/java/epam/springboot/advanced/demo/DemoApplication.java
+++ src/main/java/epam/springboot/advanced/demo/DemoApplication.java
@@ -1,6 +1,5 @@
 package epam.springboot.advanced.demo;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
